1752f5f9780812f05e634f7145b596bf3c2eae32
kdchoi-mkt <valuekds@naver.com>
2021-01-03 21:51:51 +0900

    initial commit

--- kaggle-riiid/preprocessing/preprocessing.py
+++ kaggle-riiid/preprocessing/preprocessing.py
@@ -2,7 +2,10 @@ import pandas as pd
 import numpy as np
 
 def preprocessing(data_frame: pd.DataFrame) -> pd.DataFrame:
-    """Preprocess users' learning behavior to analyze with machine learning."""
+    """Preprocess users' learning behavior to analyze with machine learning.
+    
+    The input data is form of log data (Panel Data), but the output data will be the form of cross-sectional data.
+    """
 
     data_frame = derive_df_lecture_info(data_frame)
     data_frame = derive_df_question_info(data_frame)
