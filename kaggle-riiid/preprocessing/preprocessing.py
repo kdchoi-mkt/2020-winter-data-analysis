0d4d2dc576766cf4fbec61725477e60cd248d3e0
kdchoi-mkt <valuekds@naver.com>
2021-01-03 22:01:42 +0900

    fill out derive_df_user_info

--- kaggle-riiid/preprocessing/preprocessing.py
+++ kaggle-riiid/preprocessing/preprocessing.py
@@ -9,3 +9,14 @@ def preprocessing(data_frame: pd.DataFrame) -> pd.DataFrame:
     data_frame = derive_df_user_info(data_frame)
 
     return data_frame
+
+def derive_df_user_info(data_frame: pd.DataFrame) -> pd.DataFrame:
+    """Feature engineering the user information with regard to the user's question answered history.
+
+    The feature has the following:
+    1. The answered count for each Part in the TOEIC
+    2. The correction count for each Part in the TOEIC
+    ...
+    """
+    
+    return data_frame
